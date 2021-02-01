858e8c131bbf94d2c8ec78a8b94f808904772d60
Sam Hearst <samhearst1013@gmail.com>
2021-02-01 17:45:17 -0500

    user features

---
+++ db/models/users.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    firstName: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      unique: true,
+      allowNull: false
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bcrypt = require('bcrypt');
+const { asyncHandler, csrfProtection, handleValidationErrors } = require("../utils");
+const { check } = require("express-validator");
+const { User } = require("../db/models");
+// const { User } = db;
+const { loginUser, logoutUser } = require("../auth");
+const router = express.Router();
 
+// console.log(User);
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', asyncHandler( async (req, res, next) => {
+    const users = await User.findAll();
+    console.log(req.session.auth);
+    res.render('users', {users, title: "im working" });
+}));
+
+
 
 module.exports = router;
