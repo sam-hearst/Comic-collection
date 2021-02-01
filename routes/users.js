b350c7700dd0c033f9ad6149467cffa42234374f
dornkholland <dornkholland@gmail.com>
2021-02-01 16:40:14 -0700

    Added backend framework for registering user, started front end of form

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,60 @@ const { User } = require("../db/models");
 const { loginUser, logoutUser } = require("../auth");
 const router = express.Router();
 
+const userValidators = [
+  check("firstName")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for First Name")
+    .isLength({ max: 50 })
+    .withMessage("First Name must not be more than 50 characters long"),
+  check("lastName")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for Last Name")
+    .isLength({ max: 50 })
+    .withMessage("Last Name must not be more than 50 characters long"),
+  check("email")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for Email Address")
+    .isLength({ max: 255 })
+    .withMessage("Email Address must not be more than 255 characters long")
+    .isEmail()
+    .withMessage("Email Address is not a valid email"),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for Password")
+    .isLength({ max: 50 })
+    .withMessage("Password must not be more than 50 characters long")
+    .matches(/^(?=._[a-z])(?=._[A-Z])(?=._[0-9])(?=._[!@#$%^&*])/, "g")
+    .withMessage(
+      'Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'
+    )
+    .custom((value) => {
+      return User.findOne({ where: { email: value } }).then((user) => {
+        if (user) {
+          return Promise.reject(
+            "The provided Email Address is already in use by another account"
+          );
+        }
+      });
+    }),
+  check("confirmPassword")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for Confirm Password")
+    .isLength({ max: 50 })
+    .withMessage("Confirm Password must not be more than 50 characters long")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Confirm Password does not match Password");
+      }
+      return true;
+    }),
+];
+
+
+
+
+
+
 // console.log(User);
 /* GET users listing. */
 router.get('/', asyncHandler( async (req, res, next) => {
@@ -14,6 +68,48 @@ router.get('/', asyncHandler( async (req, res, next) => {
     console.log(req.session.auth);
     res.render('users', {users, title: "im working" });
 }));
+
+router.get('/new', csrfProtection, (req, res) => {
+    res.render('signup', { csrfToken: req.csrfToken(), user: {} });
+    });
+
+router.post( "/new", csrfProtection, userValidators, asyncHandler(async (req, res) => {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmedPassword,
+    } = req.body;
+
+
+    const validatorErrors = validationResult(req);
+​
+        if (validatorErrors.isEmpty()) {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const user = await User.create({
+                firstName,
+                lastName,
+                email,
+                hashedPassword,
+            });
+            loginUser(req, res, user);
+            res.redirect('/');
+        } else {
+            const errors = validatorErrors.array().map((error) => {
+                console.log(error.msg);
+                return error.msg;
+            });
+            const user = { firstName, lastName, email };
+            res.render('signup', {
+                title: 'Sign Up',
+                user,
+                errors,
+                csrfToken: req.csrfToken(),
+            });
+        }
+    })
+);
 
 
 
