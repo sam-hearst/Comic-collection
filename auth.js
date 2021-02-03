1ed08b8d0f9dcaa99d9be0084790349ef7d6fdff
dornkholland <dornkholland@gmail.com>
2021-02-02 19:56:22 -0700

    Completed login/logout functionality and dynamic profile buttons.

--- auth.js
+++ auth.js
@@ -4,8 +4,34 @@ const loginUser = (req, res, user) => {
     req.session.auth = { userId: user.id, email: user.email }
 };
 
+const restoreUser = async (req, res, next) => {
+  // Log the session object to the console
+  // to assist with debugging.
+  console.log(req.session)
+  if (req.session.auth) {
+    const { userId } = req.session.auth;
+
+    try {
+      const user = await User.findByPk(userId);
+
+      if (user) {
+        res.locals.authenticated = true;
+        res.locals.user = user;
+        next();
+      }
+    } catch (err) {
+      res.locals.authenticated = false;
+      next(err);
+    }
+  } else {
+    res.locals.authenticated = false;
+    next();
+  }
+};
+
+
 const logoutUser = (req, res) => {
-    delete req.session.auth
+    delete req.session.auth;
 }
 
-module.exports = { loginUser, logoutUser };
+module.exports = { loginUser, logoutUser, restoreUser };
