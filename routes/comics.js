8b31a9c23ebbade777aa782bb0acbc2a16a40e71
dornkholland <dornkholland@gmail.com>
2021-02-03 10:55:58 -0700

    started layout of comic page

---
+++ routes/comics.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { csrfProtection, asyncHandler } = require("../utils");
+
+const { Comic } = require("../db/models");
+
+router.get("/:id", asyncHandler(async (req, res, next) => {
+  const comic = await Comic.findByPk(parseInt(req.params.id, 10));
+  res.render("comic", { comic });
+}));
+
+
