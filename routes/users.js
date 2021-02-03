6ef238483244b9ea346a0b1c9c59f6eb86d02441
leoworkcp <leoworkcp@gmail.com>
2021-02-03 01:56:40 -0500

    fix the auth issue we had, now the session should be created and deleted correctly, that log-in/log-out UI feature is working now!

--- routes/users.js
+++ routes/users.js
@@ -68,13 +68,18 @@ router.get(
   "/",
   asyncHandler(async (req, res, next) => {
     const users = await User.findAll();
-    console.log(req.session.auth);
+    // console.log(req.session.auth);
+    res.json({ users });
     res.render("users", { users, title: "im working" });
   })
 );
 
+router.get("/signup", csrfProtection, (req, res) => {
+  res.render("signup", { csrfToken: req.csrfToken(), user: {} });
+});
+
 router.post(
-  "/",
+  "/signup",
   csrfProtection,
   userValidators,
   asyncHandler(async (req, res) => {
@@ -108,47 +113,68 @@ router.post(
 );
 
 const loginValidators = [
-    check("email")
-        .exists({ checkFalsy: true })
-        .withMessage("Please provide a value for Email Address"),
-    check("password")
-        .exists({ checkFalsy: true })
-        .withMessage("Please provide a value for Password")
+  check("email")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for Email Address"),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for Password"),
 ];
 
-router.post('/login', csrfProtection, loginValidators, asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
-  let errors = [];
+router.get("/login", csrfProtection, (req, res) => {
+  res.render("login", { csrfToken: req.csrfToken(), userName: "" });
+});
 
-  const validatorErrors = validationResult(req);
+router.post(
+  "/login",
+  csrfProtection,
+  loginValidators,
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    let errors = [];
 
-  if (validatorErrors.isEmpty()) {
-    // TODO Attempt to login the user.
-    // redirect
-    const user = await User.findOne({ where: { email }})
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      // TODO Attempt to login the user.
+      // redirect
+      const user = await User.findOne({ where: { email } });
       if (user !== null) {
-        const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+        const passwordMatch = await bcrypt.compare(
+          password,
+          user.hashedPassword.toString()
+        );
         if (passwordMatch) {
           loginUser(req, res, user);
-          return res.redirect('/');
+          return res.redirect("/");
         }
       }
-      errors.push('Login failed for the provided email address and password');
-  } else {
-    errors = validatorErrors.array().map((error) => error.msg);
-  }
+      errors.push("Login failed for the provided email address and password");
+    } else {
+      errors = validatorErrors.array().map((error) => error.msg);
+    }
 
-    res.render('login', {
-      title: 'Login',
+    res.render("login", {
+      title: "Login",
       email,
       errors,
       csrfToken: req.csrfToken(),
     });
-}));
+  })
+);
 
 router.post("/logout", (req, res) => {
   logoutUser(req, res);
-  res.redirect('/login');
-})
+  res.redirect("/");
+});
+
+// router.get(
+//   "/:id",
+//   asyncHandler(async (req, res) => {
+//     const user = await User.findOne({ where: { id: req.params.id } });
+//     console.log(user.id);
+//     console.log(user.firstName);
+//   })
+// );
 
 module.exports = router;
