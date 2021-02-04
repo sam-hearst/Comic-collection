1c37e81a948352661da16d92faa01ab1d4538b26
Sam Hearst <samhearst1013@gmail.com>
2021-02-03 23:58:37 -0500

    adding styling plus some route stuff

--- routes/comics.js
+++ routes/comics.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { csrfProtection, asyncHandler } = require("../utils");
+const { requireAuth } = require('../auth'); // wanna require for all things where user has to be logged in
 
 const { Comic } = require("../db/models");
 
-router.get("/:id", asyncHandler(async (req, res, next) => {
+router.get("/:id(\\d+)", asyncHandler(async (req, res, next) => {
   const comic = await Comic.findByPk(parseInt(req.params.id, 10));
   res.render("comic", { comic });
 }));
