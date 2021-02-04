8aad58b18ef3f195f831d2e3a37860fdc69ba9cb
Sam Hearst <samhearst1013@gmail.com>
2021-02-03 19:37:30 -0500

    working more on the homepage

--- db/seeders/20210202212119-comicTestData.js
+++ db/seeders/20210202212119-comicTestData.js
@@ -41,9 +41,21 @@ module.exports = {
           publishDate: new Date("2017-02-28"),
           pages: "170",
           available: true,
-            imageUrl: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/439378/439378._SX360_CLs%7C360,553%7Ccu/cmx-cu-sash-lg.png%7C0,0,361,554%20208,401,152,152_QL80_TTD_.jpg",
+          imageUrl: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/439378/439378._SX360_CLs%7C360,553%7Ccu/cmx-cu-sash-lg.png%7C0,0,361,554%20208,401,152,152_QL80_TTD_.jpg",
           createdAt: new Date(),
           updatedAt: new Date(),
+        },
+        {
+            title: "Cyberpunk",
+            author: "Cullen Bunn",
+            price: "1.99",
+            publisher: "Dark Horse",
+            publishDate: new Date("2020-09-09"),
+            pages: "22",
+            available: true,
+            imageUrl: "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/882762/882762._SX360_QL80_TTD_.jpg",
+            createdAt: new Date(),
+            updatedAt: new Date(),
         }
       ], {});
   },
