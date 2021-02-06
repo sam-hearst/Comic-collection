fb5d8ed415f47f97c9ee298adfbb332967aaf50a
Kevin <pitzer.kevinj@gmail.com>
2021-02-06 18:14:28 -0500

    added form in collections

--- routes/collections.js
+++ routes/collections.js
@@ -26,6 +26,25 @@ router.get("/", requireAuth, asyncHandler(async (req, res) => {
 }))
 
 
+router.post("/custom", asyncHandler(async (req, res) => {
+    const collectionName = req.body.custom;
+    const userId = req.session.auth.userId;
+    // bootleg: prepopulate the collection with a random comic because it is too
+    //     late to update our database to include a table with collection names
+    const comicId = Math.floor(Math.random() * 20)
+    
+    await Collection.create({
+        name: collectionName,
+        userId,
+        comicId,
+        readStatus: true
+    })
+
+    // res.render('collection');
+
+}))
+
+
 /*
 Read Status Collection names:
 Want to Read => wantToRead
@@ -33,26 +52,26 @@ Currently Reading
 Read
 */
 // STILL WORKING ON ADDING A COMIC TO A COLLECTION
-async function addComicToCollection(userId, comicId, collectionName) {
+// async function addComicToCollection(userId, comicId, collectionName) {
 
-    await Collection.create({
-        name: "reading",
-        userId,
+//     await Collection.create({
+//         name: "reading",
+//         userId,
 
-    })
-}
+//     })
+// }
 
-router.post('/:name', requireAuth, asyncHandler(async (req, res) => {
-    // const comicId = parseInt(req.params.id, 10);
-    const collectionName = req.params.name;
+// router.post('/:name', requireAuth, asyncHandler(async (req, res) => {
+//     // const comicId = parseInt(req.params.id, 10);
+//     const collectionName = req.params.name;
     
-    console.log('sent collection: ', req.body.collection)
-    console.log('collectionName: ', collectionName)
-    // console.log('comicId: ', comicId)
-    console.log('userId: ', userId)
-    // addComicToCollection(userId, comicId, collectionName)
-    res.json({  });
-}))
+//     console.log('sent collection: ', req.body.collection)
+//     console.log('collectionName: ', collectionName)
+//     // console.log('comicId: ', comicId)
+//     console.log('userId: ', userId)
+//     // addComicToCollection(userId, comicId, collectionName)
+//     res.json({  });
+// }))
 
 
 module.exports = router;
