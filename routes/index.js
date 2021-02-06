1c8f7f4ecd98f427b9517234a84976b8957e488a
Sam Hearst <samhearst1013@gmail.com>
2021-02-06 17:49:43 -0500

    side bar functionality

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { csrfProtection, asyncHandler } = require("../utils");
 const db = require("../db/models");
+const { Comic } = db;
+const { User } = db;
+
 
 /* GET home page. */
 router.get(
@@ -32,7 +35,26 @@ router.get("/login", csrfProtection, (req, res) => {
 });
 
 router.get('/test/sidebar', csrfProtection, asyncHandler(async (req, res) => {
-    res.render('sidebar', { csrfToken: req.csrfToken() });
+    let userId = null;
+    if (req.session.auth) {
+        userId = req.session.auth.userId;
+    }
+
+    const collections = await db.Collection.findAll({
+        where: {
+            userId
+        },
+        include: [User, Comic]
+    })
+    let collectionArray = []
+    for (let collection of collections) {
+        collectionArray.push(collection.name)
+    }
+    const collectionNames = [...new Set(collectionArray)];
+
+    console.log(collections);
+
+    res.render('sidebar', { collections, collectionNames, csrfToken: req.csrfToken() });
 }))
 
 module.exports = router;
