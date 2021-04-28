981d7c6a0a27ec14fc627a40d000b803d1a00418
leoworkcp <leoworkcp@gmail.com>
2021-04-28 17:48:03 -0400

    searchBar style fix and hide funcionality too

--- db/migrations/20210202211231-create-comics.js
+++ db/migrations/20210202211231-create-comics.js
@@ -1,65 +1,65 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Comics', {
+    return queryInterface.createTable("Comics", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
         allowNull: false,
-        type: Sequelize.STRING(200)
+        type: Sequelize.STRING(200),
       },
       author: {
         allowNull: false,
-        type: Sequelize.STRING(75)
+        type: Sequelize.STRING(75),
       },
       description: {
         allowNull: false,
-        type: Sequelize.STRING(10000)
+        type: Sequelize.STRING(10000),
       },
       price: {
         allowNull: false,
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL,
       },
       publisher: {
         allowNull: false,
-        type: Sequelize.STRING(100)
+        type: Sequelize.STRING(100),
       },
       publishDate: {
         allowNull: false,
-        type: Sequelize.DATEONLY
+        type: Sequelize.DATEONLY,
       },
       pages: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       available: {
         allowNull: false,
         type: Sequelize.BOOLEAN,
-        defaultValue: true
+        defaultValue: true,
       },
       imageUrl: {
-        type: Sequelize.STRING(255)
+        type: Sequelize.STRING(255),
       },
       backgroundImageUrl: {
-        type: Sequelize.STRING(255)
+        type: Sequelize.STRING(255),
       },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.fn('now')
+        defaultValue: Sequelize.fn("now"),
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.fn('now')
-      }
+        defaultValue: Sequelize.fn("now"),
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Comics');
-  }
+    return queryInterface.dropTable("Comics");
+  },
 };
