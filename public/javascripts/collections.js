a5b981bf0f95e34bc99d824d7b9b6dc1a9e90736
Kevin <pitzer.kevinj@gmail.com>
2021-04-29 11:29:35 -0400

    add edit form and sidebar.js file

--- public/javascripts/collections.js
+++ public/javascripts/collections.js
@@ -1,54 +1,9 @@
-
-// // query the database and update the collection table for the specific comic and user
-// const updateCollections = async (comicId, route) => {
-//     console.log("hitting update route!!")
-//     const response = await fetch(`/api/collections/${route}/comics/${comicId}`, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify({
-//             collection: "Want to Read"
-//         })
-//     });
-
-//     if (response.ok) {
-//         const data = await response.json()
-//         return data.comic;
-//     }
-// }
-
-// // side bar collection event
-// const collections = document.querySelector(".shelf-content")
-// collections.addEventListener('click', async (e) => {
-//     if (e.target.type === "button") {
-//         const collection = e.target.value.split(' ').join('-');
-//         const comicId = e.target.id
-
-//         const comic = await updateCollections(comicId, collection);
-//         // change the side bar inner HTML here
-//         // This logic is for adding a comic through AJAX
-//         const rightCollection = e.target.value.split(' ').join('-').toLowerCase();
-//         const imgContainers = document.getElementsByClassName("sidebar__img-container")
-//         for (let i = 0; i < imgContainers.length; i++) {
-//             let imgContainer = imgContainers[i];
-//             if (imgContainer.childElementCount < 4 && imgContainer.id === rightCollection) {
-//                 const aTag = document.createElement('a')
-//                 aTag.setAttribute("href", `/comics/${comic.id}`)
-//                 aTag.innerHTML = `<img class="sidebar__img" src=${comic.imageUrl}>`
-//                 imgContainer.appendChild(aTag);
-//             }
-//         }
-//     }
-// })
-
-// collections route collection events
+// Delete a collection
 const deleteCollectionBtns = [...document.getElementsByClassName('collection--delete')]
-const editCollectionBtns = [...document.getElementsByClassName('collection--edit')]
 
-// function for deleting a collection
 const deleteCollection = async (e) => {
-    const collectionName = e.target.id;
+    const collectionName = e.target.id.split('-')[1];
+    console.log('collectionName: ', collectionName)
     const res = await fetch(`/api/collections`, {
         method: 'DELETE',
         headers: {
@@ -61,33 +16,68 @@ const deleteCollection = async (e) => {
     if (data.status === 200) {
         // delete the collection container
         const deletedCollection = document.getElementById(`scroll-${collectionName}`);
-        // const deletedSection = deletedCollection.parentNode.parentNode.parentNode.parentNode
         deletedCollection.remove();
         // delete the collection name in the top list of names
         const deletedName = document.getElementById(`list-${collectionName}`)
         deletedName.remove();
-        // deletedSection.innerHTML = '';
-
     }
-
 }
 
 deleteCollectionBtns.forEach(btn => {
     btn.addEventListener('click', deleteCollection);
 })
 
+// Edit a collection title
+const editCollectionBtns = [...document.getElementsByClassName('collection--edit')]
+
+const cancelEditCollection = async (e) => {
+    e.preventDefault();
+    const thisForm = e.target.parentNode.parentNode;
+    thisForm.remove();
+    const collName = e.target.id.split('-')[1]
+    const editId = `headerEdit-${collName}`
+    const editButton = document.getElementById(editId)
+    editButton.removeAttribute("disabled")
+}
+
+const submitEditCollection = async (e) => {
+
+}
+
+
+const editCollection = async (e) => {
+    const clickedButton = e.target
+    const currentName = clickedButton.id.split('-')[1]
+    const headerDiv = clickedButton.parentNode.parentNode.parentNode
+    if (!clickedButton.hasAttribute("disabled")) {
+        clickedButton.setAttribute("disabled", "");
+        const editContainer = document.createElement('form');
+        editContainer.setAttribute("class", "edit__collection-form")
+        editContainer.innerHTML = `
+            <label for="editCollName">Update Collection Name</label>
+            <input name="editCollName" required />
+            <div>
+                <button id="collEdit-${currentName}" class="editColl--save">Save Changes </button>
+                <button id="collCancel-${currentName}" class="editColl--cancel">Cancel Changes</button>
+            </div>
+        `
+        headerDiv.appendChild(editContainer)
+
+        document.getElementById(`collCancel-${currentName}`).addEventListener('click', cancelEditCollection);
+        document.getElementById(`collEdit-${currentName}`).addEventListener('click', submitEditCollection);
+    }
+}
+
+editCollectionBtns.forEach(btn => {
+    btn.addEventListener('click', editCollection)
+})
+
 // events to scroll each collection into view
 const collectionNames = [...document.getElementsByClassName('collection__list-name')]
 collectionNames.forEach(name => {
     name.addEventListener('click', () => {
-        console.log('the name in the listener: ', `scroll-${name.innerHTML}`)
         document.getElementById(`scroll-${name.innerHTML}`).scrollIntoView({
             behavior: "smooth"
         })
     })
 })
-// const overviewClick = () => {
-//     document.getElementById('overview').scrollIntoView({
-//         behavior: "smooth"
-//     });
-// }
---
+++ public/javascripts/sidebar.js
@@ -0,0 +1,42 @@
+// query the database and update the collection table for the specific comic and user
+const updateCollections = async (comicId, route) => {
+    console.log("hitting update route!!")
+    const response = await fetch(`/api/collections/${route}/comics/${comicId}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            collection: "Want to Read"
+        })
+    });
+
+    if (response.ok) {
+        const data = await response.json()
+        return data.comic;
+    }
+}
+
+// side bar collection event
+const collections = document.querySelector(".shelf-content")
+collections.addEventListener('click', async (e) => {
+    if (e.target.type === "button") {
+        const collection = e.target.value.split(' ').join('-');
+        const comicId = e.target.id
+
+        const comic = await updateCollections(comicId, collection);
+        // change the side bar inner HTML here
+        // This logic is for adding a comic through AJAX
+        const rightCollection = e.target.value.split(' ').join('-').toLowerCase();
+        const imgContainers = document.getElementsByClassName("sidebar__img-container")
+        for (let i = 0; i < imgContainers.length; i++) {
+            let imgContainer = imgContainers[i];
+            if (imgContainer.childElementCount < 4 && imgContainer.id === rightCollection) {
+                const aTag = document.createElement('a')
+                aTag.setAttribute("href", `/comics/${comic.id}`)
+                aTag.innerHTML = `<img class="sidebar__img" src=${comic.imageUrl}>`
+                imgContainer.appendChild(aTag);
+            }
+        }
+    }
+})
